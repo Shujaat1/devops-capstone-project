c3b0bad328460da08c66633ccc90d8eb895129f9
H M Shujaat Zaheer <50784529+Shujaat1@users.noreply.github.com>
2025-11-26 00:25:44 -0500

    Fix URL formatting in test for reading an account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -125,7 +125,8 @@ class TestAccountService(TestCase):
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
         resp = self.client.get(
-            f"{BASE_URL}/{account.id}",
+            BASE_URL.
+          /{account.id}",
             content_type="application/json"
         )
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
