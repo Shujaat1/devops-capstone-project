7d1e5362e2ee4bb47b022eba2bf2e56319a9244c
H M Shujaat Zaheer <50784529+Shujaat1@users.noreply.github.com>
2025-11-26 00:36:55 -0500

    Remove account endpoint implementations
    Removed the implementations of account-related endpoints, leaving placeholders for future code.

--- service/routes.py
+++ service/routes.py
@@ -61,64 +61,21 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
-@app.route("/accounts", methods=["GET"])
-def list_accounts():
-    """
-    List all Accounts
-    This endpoint will list all Accounts
-    """
-    app.logger.info("Request to list Accounts")
-    
-    accounts = Account.all()
-    account_list = [account.serialize() for account in accounts]
-    
-    app.logger.info("Returning %d accounts", len(account_list))
-    return account_list, status.HTTP_200_OK
-```
-
+# ... place you code here to LIST accounts ...
 
 
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
 
-@app.route("/accounts/<int:account_id>", methods=["GET"])
-def read_account(account_id):
-    """
-    Reads an Account
-    This endpoint will read an Account based on the account_id
-    """
-    app.logger.info("Request to read an Account with id: %s", account_id)
-    
-    account = Account.find(account_id)
-    if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
-    return account.serialize(), status.HTTP_200_OK
+# ... place you code here to READ an account ...
 
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-@app.route("/accounts/<int:account_id>", methods=["PUT"])
-def update_account(account_id):
-    """
-    Update an Account
-    This endpoint will update an Account based on the posted data
-    """
-    app.logger.info("Request to update an Account with id: %s", account_id)
-    
-    account = Account.find(account_id)
-    if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
-    account.deserialize(request.get_json())
-    account.update()
-    
-    return account.serialize(), status.HTTP_200_OK
-```
-
+# ... place you code here to UPDATE an account ...
 
 
 ######################################################################
