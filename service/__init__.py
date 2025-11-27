e0b8eda1facc0f53f55e32fd029c97eae2b4fd66
Shujaat1 <shujabis@gmail.com>
2025-11-26 23:05:49 -0500

    Fixed error handler registration for DataValidationError

--- service/__init__.py
+++ service/__init__.py
@@ -4,6 +4,7 @@ Package for the application models and service routes
 This module creates and configures the Flask app and sets up the logging
 and SQL database
 """
+import sys
 from flask import Flask
 from flask_talisman import Talisman
 from flask_cors import CORS
@@ -20,8 +21,12 @@ talisman = Talisman(app)
 # Initialize CORS
 CORS(app)
 
-# Import routes after app is created
-from service import routes  # noqa: E402, F401
+# Import the routes After the Flask app is created
+from service import routes, models  # noqa: F401 E402 pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
+from service.common import error_handlers  # noqa: F401 E402 pylint: disable=wrong-import-position
+
+# Initialize error handlers
+error_handlers.init_error_handlers(app)
 
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
@@ -29,5 +34,12 @@ log_handlers.init_logging(app, "gunicorn.error")
 app.logger.info(70 * "*")
 app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
 app.logger.info(70 * "*")
+
+try:
+    models.init_db(app)  # make our sqlalchemy tables
+except Exception as error:  # pylint: disable=broad-except
+    app.logger.critical("%s: Cannot continue", error)
+    # gunicorn requires exit code 4 to stop spawning workers when they die
+    sys.exit(4)
 
 app.logger.info("Service initialized!")
